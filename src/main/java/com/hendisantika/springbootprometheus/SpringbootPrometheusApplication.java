6379974531ca18c50bd79edfd2907b160a49b3bf
hendisantika <hendisantika@yahoo.co.id>
2019-01-24 07:57:46 +0700

    #3 - Add Prometheus Annotation

--- src/main/java/com/hendisantika/springbootprometheus/SpringbootPrometheusApplication.java
+++ src/main/java/com/hendisantika/springbootprometheus/SpringbootPrometheusApplication.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootprometheus;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class SpringbootPrometheusApplication {
 
     public static void main(String[] args) {
